35b32ed2f5a63c78325dec0abc9db4af7d32d216
Filip Stankovic <filips@gmail.com>
2016-02-15 19:45:28 -0500

    Finish game board draw

--- js-src/drawgameboard.js
+++ js-src/drawgameboard.js
@@ -2,53 +2,53 @@ function drawGameBoard(boardSize, magicArray) {
   // Clear the board first
   deleteContainer();
 
-    // Below was not a good idea (or I didn't execute it properly). The element must be on page for jQ selectors to work. It's also adding complexity for no reason.
-    // var containerSelect = $('body .container')
-    // var tableSelect = $('body .container table')
-    // var rowSelect = $('body .container table tr')
+  var magicArray = magicArray;
 
-    // Draw out the table calling appropriate functions
-    $('body .container').append(drawTable());
-    $('body .container table').append(drawRows(boardSize));
-    drawCells(boardSize);
+  // Below was not a good idea (or I didn't execute it properly). The element must be on page for jQ selectors to work. It's also adding complexity for no reason.
+  // var containerSelect = $('body .container')
+  // var tableSelect = $('body .container table')
+  // var rowSelect = $('body .container table tr')
 
-    function drawTable() {
-      return "<table></table>";
+  // Draw out the table calling appropriate functions
+  $('body .container').append(drawTable());
+  $('body .container table').append(drawRows(boardSize));
+  drawCells(boardSize);
+
+  function drawTable() {
+    return "<table></table>";
+  }
+
+  function drawRows(boardSize) {
+    var tableRows = "";
+
+    for (i = 1; i < (boardSize + 1); i++) {
+      tableRows += "<tr id='row-" + i + "'></tr>";
     }
 
-    function drawRows(boardSize) {
-      var tableRows = "";
+    // Return right # of trs with unique id
+    return tableRows;
+  }
 
-      for (i = 1; i < (boardSize + 1); i++) {
-        tableRows += "<tr id='row-" + i + "'></tr>";
-      }
+  function drawCells(boardSize) {
+    var cellCount = 1;
+    var magicCount = 0;
 
-      // Return right # of trs with unique id
-      return tableRows;
+    for (i = 1; i < (boardSize + 1); i++) {
+      updateRow(i, boardSize);
     }
 
-    function drawCells(boardSize) {
-      var cellCount = 1;
-      var totalCells = Math.pow(boardSize, 2);
+    function updateRow(currentRow, boardSize) {
+      rowSelector = "#row-" + currentRow;
 
-      for (i = 1; i < (boardSize + 1); i++) {
-        updateRow(i);
-      }
-
-      function updateRow(currentRow) {
-        rowSelector = "#row-" + currentRow;
-
-        // There is repitition here that could be refactored out (somehow) ;)
-        for (i = 1; i < (boardSize + 1); i++) {
-          $(rowSelector).append(drawCell());
-          cellCount++;
-        }
-
-        function drawCell() {
-          return "<td id='cell-" + cellCount +"'></td>"
-        }
+      // There is repetition here that could be refactored out
+      for (j = 1; j < (boardSize + 1); j++) {
+        var cellContent = "<td id='cell-" + cellCount + "' data-magic-squares='" + magicArray[magicCount] + "'></td>";
+        $(rowSelector).append(cellContent);
+        cellCount++;
+        magicCount++;
       }
     }
+  }
 }
 
 // This function should store the appropriate magic square value to data-magic-squares for each cell in order
